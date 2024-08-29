8731aecdc8a3725d93c25d84b47e3371227d7628
seunghun <tmdgns5945@oasis.inje.ac.kr>
2024-08-29 19:04:44 +0900

    feat: test

--- src/components/DetailModal.jsx
+++ src/components/DetailModal.jsx
@@ -95,8 +95,8 @@ const DetailModal = ({ performance, onClose }) => {
         objectType: "feed",
         content: {
           title: "공연정보는 역시 콘서트스팟!",
-          text: `'${performance.prfnm}' 공연 예매 링크: ${ticketUrl}\n\n친구가 같이 보자고 하는 공연이군요! 콘서트스팟에서 더 많은 공연을 확인해보세요.`,
-          description: "더 자세한 정보는 콘서트스팟 웹에서 확인하세요",
+          text: "???",
+          description: `'${performance.prfnm}' 공연 예매 링크: ${ticketUrl}\n\n친구가 같이 보자고 하는 공연이군요! 콘서트스팟에서 더 많은 공연을 확인해보세요.`,
           imageUrl: `${performance.poster}`,
           link: {
             mobileWebUrl: sharedUrl,
