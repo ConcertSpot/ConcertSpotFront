371008eb02f034a168379a67e3e1dcf4f33567cc
seunghun <tmdgns5945@oasis.inje.ac.kr>
2024-08-29 15:34:21 +0900

    feat: final

--- src/components/DetailModal.jsx
+++ src/components/DetailModal.jsx
@@ -88,11 +88,23 @@ const DetailModal = ({ performance, onClose }) => {
 
   const handleShare = () => {
     if (performance.relates && performance.relates[0] && performance.relates[0].relate[0].relateurl[0]) {
-      window.Kakao.Link.sendScrap({
-        requestUrl: performance.relates[0].relate[0].relateurl[0],
+      window.Kakao.Link.sendCustom({
+        templateId: 111634,  // 여기에 템플릿 ID를 사용하세요.
+        templateArgs: {
+          'title': performance.prfnm,
+          'description': `${performance.genrenm} - ${performance.fcltynm}`,
+          'imageUrl': performance.poster,
+          'linkUrl': performance.relates[0].relate[0].relateurl[0]
+        }
+      }).then(() => {
+        console.log('공유 성공');
+      }).catch(error => {
+        console.error("카카오톡 공유에 실패했습니다.", error);
+        alert("카카오톡 공유에 실패했습니다. 잠시 후 다시 시도해주세요.");
       });
     } else {
       console.error("공연 링크가 없습니다.");
+      alert("공연 링크가 존재하지 않습니다.");
     }
   };
 
@@ -126,7 +138,7 @@ const DetailModal = ({ performance, onClose }) => {
           <span style={{ color: "gray", fontWeight: "bold" }}>날짜 </span>
           <span style={{ marginTop: "0px" }}>{performance.prfpdfrom} ~ {performance.prfpdto}</span>
 
-          <span style={{ color: "gray", fontWeight: "bold" }}>출현진 </span>
+          <span style={{ color: "gray", fontWeight: "bold" }}>출연진 </span>
           <span style={{ marginTop: "0px" }}>{performance.prfcast}</span>
 
           <span style={{ color: "gray", fontWeight: "bold" }}>제작진 </span>
